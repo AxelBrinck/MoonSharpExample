09ab1b0e3c63750202fcf0ae36f9cd94e9001b82
Axel Brinck <axel.louis.1991@gmail.com>
2020-11-11 19:53:24 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonoRunnerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
