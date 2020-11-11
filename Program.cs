4e72effcb98fcda415b2c1ec057b14420fbdd1a1
Axel Brinck <axel.louis.1991@gmail.com>
2020-11-11 20:25:15 +0100

    inserted lua code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using MoonSharp.Interpreter;
 
-namespace MonoRunnerExample
+namespace MoonSharpExample
 {
     class Program
     {
+        static double Factorial()
+        {
+            string script = @"    
+                -- defines a factorial function
+                function fact (n)
+                    if (n == 0) then
+                        return 1
+                    else
+                        return n*fact(n - 1)
+                    end
+                end
+
+                return fact(6)";
+
+            var res = Script.RunString(script);
+            return res.Number;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Factorial());
         }
     }
 }
